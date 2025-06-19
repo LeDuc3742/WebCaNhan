0bbbdb20964a212e9b96a223eee81b3429a65933
Le Minh Duc <leminhduc2623@gmail.com>
2025-06-20 00:08:32 +0700

    thay đổi code

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ window.addEventListener("load", function () {
     preloader.style.display = "none";
   }
 });
+
+// Lightbox Image Zoom
+document.querySelectorAll('.project-img').forEach(img => {
+  img.addEventListener('click', function () {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightbox.style.display = "block";
+    lightboxImg.src = this.src;
+  });
+});
+
+document.querySelector('.close').addEventListener('click', function () {
+  document.getElementById('lightbox').style.display = "none";
+});
+
+// Đóng lightbox khi bấm ra ngoài ảnh
+document.getElementById('lightbox').addEventListener('click', function (e) {
+  if (e.target === this) {
+    this.style.display = "none";
+  }
+});
